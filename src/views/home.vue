f9ea43002e57c27da3bff525ba5c543bd266b6da
swingxu <swingxu@foxmail.com>
2019-07-31 18:23:52 +0800

    add croppa

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,23 +2,36 @@
   <div id='home'>
     <h1>Home</h1>
     <p>deecamp 21  niubi!</p>
-      <croppa v-model="croppa" @new-image-drawn="onNewImage" @zoom="onZoom">
-      </croppa>
+    <my-image-cropper v-model="myCroppa"
+                      :width="512"
+                      :height="512"
+                      placeholder="Upload"
+                      :show-loading="true"
+                      :prevent-white-space="true"
+                      :show-remove-button="false">
+    </my-image-cropper>
+    <button @click="croppa.remove()">REMOVE</button>
   </div>
 </template>
 
 <script>
 import Vue from "vue";
-import Croppa from "vue-croppa";
-import "vue-croppa/dist/vue-croppa.css";
- 
-Vue.use(Croppa);     
+import Croppa from 'vue-croppa';
+Vue.use(Croppa, { componentName: 'my-image-cropper' });
 
 export default {
   name: 'home',
-  components: {
-    Croppa
-  },
+  methods: {
+    uploadCroppedImage() {
+      this.myCroppa.generateBlob(
+        blob => {
+          // write code to upload the cropped image file (a file is a blob)
+        },
+        'image/jpeg',
+        0.8
+      ); // 80% compressed jpeg file
+    }
+  }
 }
 </script>
 
@@ -38,4 +51,17 @@ li {
 a {
   color: #42b983;
 }
+.croppa-container {
+    margin: auto;
+    background-color: lightblue;
+    border: 2px solid grey;
+    border-radius: 8px;
+    width: 512px;
+    height: 512px;
+ }
+ .croppa-container:hover {
+   opacity: 1;
+   background-color: #f4f4f4;
+ }
+
 </style>
